635f56821d2b6b560642493ce3f35741b4ba8c02
Rami Shareef <RShareef@imohealth.com>
2021-04-03 20:54:44 -0400

    added car

--- DataApp/Models/Car.cs
+++ DataApp/Models/Car.cs
@@ -1,11 +1,27 @@
-﻿namespace DataApp.Models
+﻿using System;
+
+namespace DataApp.Models
 {
     public class Car
     {
-        public int Id { get; set; }
-        public long Price { get; set; }
-        public string Model { get; set; }
-        public string Vin { get; set; }
-        public string Manufactorer { get; set; }
+        public int RowId { get; set; }
+        public string region { get; set; }
+        public long? price { get; set; }
+        public int? year { get; set; }
+        public string manufactorer { get; set; }
+        public string model { get; set; }
+        public string condition { get; set; }
+        public string cylinders { get; set; }
+        public string fuel { get; set; }
+        public int? odometer { get; set; }
+        public string title_status { get; set; }
+        public string transmission { get; set; }
+        public string VIN { get; set; }
+        public string drive { get; set; }
+        public string size { get; set; }
+        public string type { get; set; }
+        public string paint_color { get; set; }
+        public string state { get; set; }
+        public DateTime posting_date { get; set; }
     }
 }
