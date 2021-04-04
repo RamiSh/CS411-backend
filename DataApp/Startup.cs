8aab3e656d52c0e91476b32081f95a783dddae39
ramish <rami.shareef@gmail.com>
2021-04-04 18:23:03 -0400

    correct api name

--- DataApp/Startup.cs
+++ DataApp/Startup.cs
@@ -34,7 +34,7 @@ namespace DataApp
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Team Data CS411 - Backend API specifications", Version = "v1" });
 
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
